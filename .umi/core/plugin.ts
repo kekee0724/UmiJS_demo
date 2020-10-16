af85e50bfb5ec7c03c63a3600133a5f43dad8c4e
huangb <androidleo@outlook.com>
2020-10-16 13:33:09 +0800

    Initial commit

---
+++ .umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'C:/Users/Lenovo/Desktop/备忘录/OneDrive/ReactDemo/myapp/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
+});
+
+export { plugin };
